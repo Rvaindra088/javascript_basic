c79ac1b6bd67daac5043d68e63880162a8fcc2ed
Ravindra Kumar <112094581+Rvaindra088@users.noreply.github.com>
2023-12-25 13:00:10 +0000

    basic variable

--- basic.js
+++ basic.js
@@ -1,1 +1,14 @@
-console.log("hello i am coder !")
+// console.log("hello i am coder !")
+
+var name  = "Ravindra "
+let gmail = "ravivarun099@gmail.com"
+const city = "Noida"
+let  isMarried = false 
+let age = 23
+var something;
+var x = null
+
+console.table([name,gmail,city,isMarried,age,something,x])
+
+
+
